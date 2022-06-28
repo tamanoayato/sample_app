7754cef38a820482f2cd46545a892d7d5d2f2532
ayatotamano <106876507+tamanoayato@users.noreply.github.com>
2022-06-28 03:43:50 +0000

    [finish 3-9]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,13 +4,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    #データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    #データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #詳細画面へリダイレクト
-    redirect_to list_path(list.id)
-
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -25,17 +24,23 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
-  
+
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
-    
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+
   end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
